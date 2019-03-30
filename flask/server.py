07cb5d32dc20c0e96ff6d1320362730a7c31d3ff
Rain Wang <runyu@umail.ucsb.edu>
2019-03-30 14:04:23 -0700

    Changed rent API

--- flask/server.py
+++ flask/server.py
@@ -4,7 +4,7 @@ from json import dumps
 #import sys
 #sys.path.insert(0,'/lahacks-safeLA/ML-models')
 from safeLA_predict import predictSafeLA
-from LArent_predict.py import predictRentLA
+from LArent_predict import predictRentLA
 #from flask.ext.jsonpify import jsonify
 import numpy as np
 
@@ -27,7 +27,8 @@ def getRent():
   x = request.args.get('x')
   y = request.args.get('y')
   result = predictRentLA(x, y)
-  return str(result)
+  rentavg = np.array2string(result, precision=3, separator="")
+  return str(rentavg)
 
 if __name__ == '__main__':
   app.run(debug=True, port="5002")
