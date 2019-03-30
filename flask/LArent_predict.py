bfcbb2e94ac53380debe532abfb8af4c23b01bca
s4ndhyac <sandhya2392@gmail.com>
2019-03-30 12:32:48 -0700

    Update LArent_predict.py

--- flask/LArent_predict.py
+++ flask/LArent_predict.py
@@ -12,7 +12,7 @@ def predictSafeLA(x, y):
   NN_model.add(Dense(100, input_dim=2, kernel_initializer='normal', activation='relu'))
   NN_model.add(Dense(1, kernel_initializer='normal'))
 
-  NN_model.load_weights("/Users/sandhya/Documents/repos/lahacks-safeLA/ML-models/rent_score/rentLA_weights.hdf5")
+  NN_model.load_weights("rentLA_weights.hdf5")
 
   data = array([[x,y]])
   predictions = NN_model.predict(data[0:1])
