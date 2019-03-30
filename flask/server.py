1bedce2c8376838b62190c1b62e54a43e2b4e6df
Rain Wang <runyu@umail.ucsb.edu>
2019-03-30 10:29:50 -0700

    Moved files around

--- flask/server.py
+++ flask/server.py
@@ -1,19 +1,27 @@
-
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+#import sys
+#sys.path.insert(0,'/lahacks-safeLA/ML-models')
+from safeLA_predict import predictSafeLA
+#from ML-models/safeLA_predict.py import predictSafeLA
 #from flask.ext.jsonpify import jsonify
 
 app = Flask(__name__)
 api = Api(app)
 
 class safeLA(Resource):
-	def get(self,x,y):
-		result = {'message': 'hello world'}
-		return "Hello World"
+    def get(self,x,y):
+       	result = predictSafeLA(x,y)
+        max = 0
+        for i in range(10):
+            if (result[i]>result[max]):
+                max = i
+        max = max+1
+        return max
 
 api.add_resource(safeLA, '/safeLA')
 
 if __name__ == '__main__':
-	app.run(port="5002")
+    app.run(port="5002")
