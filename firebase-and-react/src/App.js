de60764b4bc6815d516886be03021052ea502171
s4ndhyac <sandhya2392@gmail.com>
2019-03-30 14:12:15 -0700

    partial commit

--- firebase-and-react/src/App.js
+++ firebase-and-react/src/App.js
@@ -5,24 +5,42 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+      <div class="pac-card" id="pac-card">
+        <div>
+          <div id="title">
+            Autocomplete search
+          </div>
+          <div id="type-selector" class="pac-controls">
+            <input type="radio" name="type" id="changetype-all" checked="checked">
+              <label for="changetype-all">All</label>
 
-export default App;
+              <input type="radio" name="type" id="changetype-establishment">
+                <label for="changetype-establishment">Establishments</label>
+
+                <input type="radio" name="type" id="changetype-address">
+                  <label for="changetype-address">Addresses</label>
+
+                  <input type="radio" name="type" id="changetype-geocode">
+                    <label for="changetype-geocode">Geocodes</label>
+          </div>
+                  <div id="strict-bounds-selector" class="pac-controls">
+                    <input type="checkbox" id="use-strict-bounds" value="">
+                      <label for="use-strict-bounds">Strict Bounds</label>
+          </div>
+                  </div>
+                  <div id="pac-container">
+                    <input id="pac-input" type="text"
+                      placeholder="Enter a location">
+        </div>
+                  </div>
+                  <div id="map"></div>
+                  <div id="infowindow-content">
+                    <img src="" width="16" height="16" id="place-icon">
+                      <span id="place-name" class="title"></span><br>
+                        <span id="place-address"></span>
+      </div>
+                      );
+                    }
+                  }
+                  
+                  export default App;
