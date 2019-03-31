f4b8d97eaa833c07542e74d71d6473269cee6677
s4ndhyac <sandhya2392@gmail.com>
2019-03-30 22:38:31 -0700

    info display

--- firebase-and-react/src/App.js
+++ firebase-and-react/src/App.js
@@ -6,21 +6,58 @@ class App extends Component {
 
   render() {
     return (
-      <div className="pac-card" id="pac-card">
-        <div>
-          <div id="title">
-            Autocomplete search
-          </div>
+      <div>
+        <div id="info">
+
           <br />
-          <div id="pac-container">
-            <input id="pac-input" type="text"
-              placeholder="Enter a location"></input>
+
+          <div className="card">
+
+            <div className="card-body">
+              <h4 className="card-title">Projected Rent</h4>
+              <p className="card-text">
+                Find out your projected rent based on housing data from the city of LA from 2010 onwards.
+    </p>
+              <br />
+              <p id="rentLA"> </p>
+
+            </div>
           </div>
-          <div id="map"></div>
-          <div id="infowindow-content"></div>
-          <img src="" width="20" height="20" id="place-icon"></img>
-          <span id="place-name" className="title"></span><br />
-          <span id="place-address"></span>
+
+
+          <br /><br /><br /><br /><br /><br /><br /><br />
+
+          <div className="card">
+
+            <div className="card-body">
+              <h4 className="card-title">Safety Score</h4>
+              <p className="card-text">
+                Find a affordable and safe neighbourhood based on our neural network prediction using data from the city of LA.
+    </p>
+              <br />
+              <p id="safeLA"> </p>
+
+            </div>
+          </div>
+
+        </div>
+        <div className="pac-card" id="pac-card">
+          <div>
+            <div id="title">
+              Search your next home
+          </div>
+            <br />
+            <div id="pac-container">
+              <input id="pac-input" type="text"
+                placeholder="Enter a location"></input>
+            </div>
+
+            <div id="map"></div>
+            <div id="infowindow-content"></div>
+            <img src="" width="20" height="20" id="place-icon"></img>
+            <span id="place-name" className="title"></span><br />
+            <span id="place-address"></span>
+          </div>
         </div>
       </div>
 
